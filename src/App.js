d451cb2996570e7b3eb523e2becf0b0dfe41ea61
miguel bastidas <m.alejandro_80@hotmail.com>
2021-07-21 22:59:10 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 
 import React from 'react'
 import './App.scss'
+import ForeignWheather from './components/foreignWeather/ForeignWheather'
 import LastWeather from './components/lastsWeather/LastWeather'
 import MainBanner from './components/mainBanner/MainBanner'
-import Reviews from './components/reviews/Reviews'
 import VisitCard from './components/visitCard/VisitCard'
 export default function App() {
   return (
@@ -14,6 +14,7 @@ export default function App() {
         <div className='gridContainer'>
           <LastWeather />
           <VisitCard />
+          <ForeignWheather />
         </div>
       </div>
     </div>
--- src/components/foreignWeather/foreignWheather.js
+++ src/components/foreignWeather/foreignWheather.js
@@ -1,11 +1,26 @@
 /** @format */
 
 import React from 'react'
+import styles from './styles.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
-export default function foreignWheather() {
+export default function ForeignWheather() {
   return (
     <div>
-      <h1>foreignWheather</h1>
+      <div className={styles.cardContent}>
+        <div className={styles.weatherIcon}>
+          <FontAwesomeIcon icon={faMapMarkerAlt} />
+        </div>
+        <div className={styles.temperture}>
+          <h1>29</h1>
+          <span>o</span>
+          <span>c</span>
+        </div>
+        <div>
+          <span>Totam, harum doloribus?</span>
+        </div>
+      </div>
     </div>
   )
 }
